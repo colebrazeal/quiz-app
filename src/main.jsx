35530b5458eadd3a349c2904aceff08ca40b7e41
colebrazeal <cbrazeal17@gmail.com>
2024-06-18 15:20:32 -0400

    vite deployment assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Quiz from '../src/components/quiz/Quiz.jsx';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-
-export default App
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Quiz />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
